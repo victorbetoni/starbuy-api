a6b7df0b58ecfc460f0e07dcff8feec32196eb97
Victor Betoni <victorbetoni@protonmail.com>
2022-03-05 09:33:23 -0300

    Colocando conexão em um pacote separado

--- database/database.go
+++ database/database.go
@@ -1,6 +1,35 @@
 package database
 
+import (
+	"authentication-service/util"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
 type Login struct {
 	Username string `db:"username"`
 	Password string `db:"password"`
+}
+
+func Connect(db *sqlx.DB) (err error) {
+	var DBConfig util.Config
+
+	fmt.Println("Starting authentication service...")
+
+	if err = util.LoadConfig(".", &DBConfig); err != nil {
+		return
+	}
+
+	fmt.Println("Database config loaded from config file")
+
+	//Mantenha o SSLMode ativado, caso contrario ele ficara direcionando para a localhost
+	dataSource := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=require", DBConfig.HostAddress, DBConfig.Port, DBConfig.Username, DBConfig.Password, DBConfig.Schema)
+
+	db, err = sqlx.Open(DBConfig.Driver, dataSource)
+	if err != nil {
+		return err
+	}
+	return
 }
--- main.go
+++ main.go
@@ -2,34 +2,17 @@ package main
 
 import (
 	"authentication-service/database"
-	"authentication-service/util"
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
-var db *sqlx.DB
+var db sqlx.DB
 
 func main() {
 
-	var err error
-	var DBConfig util.Config
-
-	fmt.Println("Starting authentication service...")
-
-	if err := util.LoadConfig(".", &DBConfig); err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Database config loaded from config file")
-
-	//Mantenha o SSLMode ativado, caso contrario ele ficara direcionando para a localhost
-	dataSource := fmt.Sprintf("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=require", DBConfig.HostAddress, DBConfig.Port, DBConfig.Username, DBConfig.Password, DBConfig.Schema)
-
-	db, err = sqlx.Open(DBConfig.Driver, dataSource)
-
+	var err = database.Connect(&db)
 	if err != nil {
 		panic(err.Error())
 	} else {
