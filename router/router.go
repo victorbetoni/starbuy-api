e2b5ba2f40363de175c7e733d288acf94b4d9f49
Victor Betoni <victorbetoni@protonmail.com>
2022-03-05 10:14:50 -0300

    Configurando roteador

--- router/router.go
+++ router/router.go
@@ -1,8 +1,12 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"authentication-service/router/routes"
+
+	"github.com/gorilla/mux"
+)
 
 // Build vai retornar um router com as rotas configuradas
 func Build() *mux.Router {
-	return mux.NewRouter()
+	return routes.Configure(mux.NewRouter())
 }
--- router/routes/routes.go
+++ router/routes/routes.go
@@ -1,10 +1,22 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Route - Representação de todas as rotas da API
 type Route struct {
 	URI    string
 	Method string
 	Action func(http.ResponseWriter, *http.Request)
+}
+
+func Configure(router *mux.Router) *mux.Router {
+	for _, route := range loginRoutes {
+		router.HandleFunc(route.URI, route.Action).Methods(route.Method)
+	}
+
+	return router
 }
