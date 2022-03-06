ce07324da51d45a5af959de137ba845efa5d7d29
Victor Betoni <victorbetoni@protonmail.com>
2022-03-05 23:24:33 -0300

    Ajustando umas coisas

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"authentication-service/database"
 	"authentication-service/router"
+	"authentication-service/security"
 	"fmt"
 	"log"
 	"net/http"
@@ -21,6 +22,11 @@ func main() {
 	}
 
 	defer db.Close()
+
+	erroFoda := security.ComparePassword("$2a$10$wmNoJX3C9tyC4Wie9KeIj.v1P6waCEa1omgRBQNMPGFOOJESTqk/i", "lool")
+	if erroFoda != nil {
+		fmt.Println(erroFoda.Error())
+	}
 
 	router := router.Build()
 	fmt.Println("Listening and serving port ", port)
