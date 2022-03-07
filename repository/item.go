dd8a4ac571f8a0e3e18f7d49eb010d22e24838eb
Victor Betoni <victorbetoni@protonmail.com>
2022-03-07 14:06:38 -0300

    Debugando

--- repository/item.go
+++ repository/item.go
@@ -3,16 +3,19 @@ package repository
 import (
 	"authentication-service/database"
 	"authentication-service/model"
+	"fmt"
 )
 
 func DownloadItem(id string, item *model.ItemWithAssets) error {
 	db := database.GrabDB()
 
 	if err := db.Get(&item.Item, "SELECT * FROM products WHERE identifier=$1 LIMIT 1", id); err != nil {
+		fmt.Println("DEBUG 1")
 		return err
 	}
 
 	if err := db.Select(&item.Assets, "SELECT url FROM product_images WHERE product=$1", id); err != nil {
+		fmt.Println("DEBUG 2")
 		return err
 	}
 
