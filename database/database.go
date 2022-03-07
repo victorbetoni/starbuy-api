6b4efe147b3a70764f983f2e52eba45b8a61c064
Victor Betoni <victorbetoni@protonmail.com>
2022-03-07 14:36:20 -0300

    Ops

--- database/database.go
+++ database/database.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"authentication-service/util"
-	"fmt"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -16,8 +15,8 @@ func Connect() (err error) {
 	config := util.GrabConfig()
 
 	//Mantenha o SSLMode ativado, caso contrario ele ficara direcionando para a localhost
-	dataSource := fmt.Sprintf("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=require", config.HostAddress, config.Port, config.Username, config.Password, config.Schema)
+	/*dataSource := fmt.Sprintf("host=%s port=%s user=%s "+
+	"password=%s dbname=%s sslmode=require", config.HostAddress, config.Port, config.Username, config.Password, config.Schema)*/
 
 	var database *sqlx.DB
 	if database, err = sqlx.Open(config.Driver, "postgres://ntmqwvedocrexr:36074513dd76c22fc09a0eea3dc2cd12dd6a1434fc346e45a005709c0a60ffe8@ec2-34-236-87-247.compute-1.amazonaws.com:5432/d324nv8ovevvpk"); err != nil {
