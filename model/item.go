de4ac8cee04786fa3f644bdbf731c6b34275ad99
Victor Betoni <victorbetoni@protonmail.com>
2022-03-08 18:29:04 -0300

    Corrigindo estrutura do item para que o sqlx consiga ler

--- model/item.go
+++ model/item.go
@@ -11,6 +11,16 @@ type Item struct {
 }
 
 type ItemWithAssets struct {
-	Item   Item     `json:"seller"`
+	Item   Item     `json:"item"`
 	Assets []string `json:"assets"`
+}
+
+type DatabaseItem struct {
+	Identifier  string  `db:"identifier" json:"identifier,omitempty"`
+	Title       string  `db:"title" json:"title,omitempty"`
+	Seller      string  `db:"seller" json:"seller,omitempty"`
+	Price       float64 `db:"price" json:"price,omitempty"`
+	Stock       int     `db:"stock" json:"stock,omitempty"`
+	Category    int     `db:"category" json:"category,omitempty"`
+	Description string  `db:"description" json:"description,omitempty"`
 }
--- repository/item.go
+++ repository/item.go
@@ -28,15 +28,34 @@ func DownloadAllItems(items *[]model.ItemWithAssets) error {
 	}
 
 	for _, id := range ids {
-		var item model.ItemWithAssets
-		if err := db.Get(&item.Item, "SELECT * FROM products WHERE identifier=$1 LIMIT 1", id); err != nil {
+
+		//Retrieving raw database item
+		var raw model.DatabaseItem
+		if err := db.Get(&raw, "SELECT * FROM products WHERE identifier=$1 LIMIT 1", id); err != nil {
 			return err
 		}
 
-		if err := db.Select(&item.Assets, "SELECT * FROM product_images WHERE product=$1", id); err != nil {
+		//Retrieving assets
+		var assets []string
+		if err := db.Select(&assets, "SELECT url FROM product_images WHERE product=$1", id); err != nil {
 			return err
 		}
-		*items = append(*items, item)
+
+		var user model.User
+		if err := db.Get(&user, "SELECT * FROM users WHERE username=$1", raw.Seller); err != nil {
+			return err
+		}
+
+		item := model.Item{
+			Description: raw.Description,
+			Title:       raw.Title,
+			Identifier:  raw.Identifier,
+			Seller:      user,
+			Price:       raw.Price,
+			Stock:       raw.Stock,
+			Category:    raw.Category,
+		}
+		*items = append(*items, model.ItemWithAssets{item, assets})
 	}
 
 	return nil
