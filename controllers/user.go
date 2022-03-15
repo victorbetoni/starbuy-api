4e5f271bef8fc882cc0c665856b27bbabab92071
Victor Betoni <victorbetoni@protonmail.com>
2022-03-15 14:02:23 -0300

    Movendo umas coisas do register para o pacote de repositorio

--- controllers/user.go
+++ controllers/user.go
@@ -1,20 +1,13 @@
 package controllers
 
 import (
-	"database/sql"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"starbuy/authorization"
-	"starbuy/database"
 	"starbuy/model"
 	"starbuy/repository"
 	"starbuy/responses"
-	"starbuy/security"
-
-	"github.com/gorilla/mux"
 )
 
 func Register(w http.ResponseWriter, r *http.Request) {
@@ -46,50 +39,8 @@ func Register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	db := database.GrabDB()
-
-	testQueries := map[string]string{
-		fmt.Sprintf("SELECT * FROM users WHERE username='%s'", data.Username): "Username já está em uso",
-		fmt.Sprintf("SELECT * FROM users WHERE email='%s'", data.Email):       "Email já está em uso",
-	}
-
-	var found model.User
-	for key, value := range testQueries {
-		err := db.Get(&found, key)
-		if (err != nil && err != sql.ErrNoRows) || err == nil {
-			responses.Error(w, http.StatusBadRequest, errors.New(value))
-			return
-		}
-	}
-
-	tx := db.MustBegin()
-	tx.NamedExec("INSERT INTO users VALUES (:username,:email,:name,:gender,:registration,:birthdate,:seller,:profile_picture,:city)", &user)
-	if err := tx.Commit(); err != nil {
-		responses.Error(w, http.StatusInternalServerError, err)
-		return
-	}
-
-	crypt, _ := security.Hash(data.Password)
-	tx2 := db.MustBegin()
-	tx2.MustExec("INSERT INTO login VALUES ($1,$2)", data.Username, string(crypt))
-	if err := tx2.Commit(); err != nil {
-		responses.Error(w, http.StatusInternalServerError, err)
-		return
-	}
-
-	responses.JSON(w, http.StatusCreated, nil)
-}
-
-func QueryUser(w http.ResponseWriter, r *http.Request) {
-	queried := mux.Vars(r)["username"]
-	var user model.User
-
-	if err := repository.DownloadUser(queried, &user); err != nil {
-		if err == sql.ErrNoRows {
-			responses.Error(w, http.StatusNotFound, err)
-			return
-		}
-		responses.Error(w, http.StatusInternalServerError, err)
+	if err := repository.InsertUser(user, data.Password); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
 		return
 	}
 
--- repository/user.go
+++ repository/user.go
@@ -1,9 +1,46 @@
 package repository
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	"starbuy/database"
 	"starbuy/model"
+	"starbuy/security"
 )
+
+func InsertUser(user model.User, password string) error {
+	db := database.GrabDB()
+
+	testQueries := map[string]string{
+		fmt.Sprintf("SELECT * FROM users WHERE username='%s'", user.Username): "Username já está em uso",
+		fmt.Sprintf("SELECT * FROM users WHERE email='%s'", user.Email):       "Email já está em uso",
+	}
+
+	var found model.User
+	for key, value := range testQueries {
+		err := db.Get(&found, key)
+		if err != nil && err != sql.ErrNoRows {
+			return err
+		}
+		return errors.New(value)
+	}
+
+	tx := db.MustBegin()
+	tx.NamedExec("INSERT INTO users VALUES (:username,:email,:name,:gender,:registration,:birthdate,:seller,:profile_picture,:city)", &user)
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	crypt, _ := security.Hash(password)
+	tx2 := db.MustBegin()
+	tx2.MustExec("INSERT INTO login VALUES ($1,$2)", user.Username, string(crypt))
+	if err := tx2.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func DownloadUser(username string, user *model.User) error {
 	db := database.GrabDB()
