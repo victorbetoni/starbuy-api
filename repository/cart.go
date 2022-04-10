9581b928fd5d3bd0866163c5137b39f2e5938bd9
Victor Betoni <victorbetoni@protonmail.com>
2022-04-10 15:13:12 -0300

    Somando no statement

--- repository/cart.go
+++ repository/cart.go
@@ -2,8 +2,6 @@ package repository
 
 import (
 	"database/sql"
-	"encoding/json"
-	"fmt"
 	"starbuy/database"
 	"starbuy/model"
 )
@@ -33,9 +31,6 @@ func InsertCartItem(item model.RawCartItem) error {
 
 	tx := db.MustBegin()
 
-	itemM, _ := json.Marshal(item)
-	fmt.Println(string(itemM))
-
 	var recorded model.RawCartItem
 	if err := db.Get(&recorded, "SELECT * FROM shopping_cart WHERE holder=$1 AND product=$2", item.Holder, item.Item); err != nil && err == sql.ErrNoRows {
 		tx.MustExec("INSERT INTO shopping_cart VALUES ($1,$2,$3)", item.Holder, item.Item, item.Quantity)
@@ -45,15 +40,8 @@ func InsertCartItem(item model.RawCartItem) error {
 		return nil
 	}
 
-	res2B, _ := json.Marshal(recorded)
-	fmt.Println(string(res2B))
-	fmt.Printf("\nRecorded: %d\n", recorded.Quantity)
-	fmt.Printf("Incoming: %d\n", item.Quantity)
-	new := recorded.Quantity + item.Quantity
-	fmt.Printf("New: %d\n", new)
-
 	tx2 := db.MustBegin()
-	tx2.MustExec("UPDATE shopping_cart SET quantity=$1 WHERE holder=$2 AND product=$3", new, item.Holder, item.Item)
+	tx2.MustExec("UPDATE shopping_cart SET quantity=quantity+$1 WHERE holder=$2 AND product=$3", item.Quantity, item.Holder, item.Item)
 	if err := tx2.Commit(); err != nil {
 		return err
 	}
