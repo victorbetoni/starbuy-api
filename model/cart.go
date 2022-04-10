2808beda67507cfebdb082fb0cc0cb2894809526
Victor Betoni <victorbetoni@protonmail.com>
2022-04-10 15:08:55 -0300

    Corrigindo chave do banco errada

--- model/cart.go
+++ model/cart.go
@@ -3,7 +3,7 @@ package model
 type RawCartItem struct {
 	Holder   string `db:"holder" json:"holder"`
 	Quantity int    `db:"quantity" json:"quantity"`
-	Item     string `db:"item" json:"item"`
+	Item     string `db:"product" json:"item"`
 }
 
 type CartItem struct {
