5c260e9d7b5ecb13e1387bc208199f079c4de5aa
Victor Betoni <victorbetoni@protonmail.com>
2022-11-07 19:38:21 -0300

    Corrigindo nome do campo no banco de dados

--- model/review.go
+++ model/review.go
@@ -3,7 +3,7 @@ package model
 type RawReview struct {
 	User    string `db:"username" json:"user"`
 	Item    string `db:"product" json:"item"`
-	Message string `db:"msg" json:"message"`
+	Message string `db:"message" json:"message"`
 	Rate    int    `db:"rate" json:"rate"`
 }
 
