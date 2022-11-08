24aec432e7978bb18c65a842ddfda92b335cfbe9
Victor Betoni <victorbetoni@protonmail.com>
2022-11-08 20:40:50 -0300

    Não aguento mais

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -25,8 +25,11 @@ func Authorize(next util.HandlerFunc) util.HandlerFunc {
 
 func CORS(next util.HandlerFunc) util.HandlerFunc {
 	return func(c *gin.Context) (int, error) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
 		return next(c)
 	}
 }
--- router/routes/routes.go
+++ router/routes/routes.go
@@ -1,12 +1,10 @@
 package routes
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"starbuy/middleware"
 	"starbuy/util"
-	"time"
 )
 
 type AssignFunction func(*gin.Engine, gin.HandlerFunc, string)
@@ -29,21 +27,21 @@ func Configure(router *gin.Engine) *gin.Engine {
 	routes = append(routes, Order)
 	routes = append(routes, Address)
 
-	router.Use(cors.New(cors.Config{
+	/*router.Use(cors.New(cors.Config{
 		AllowMethods:     []string{"PUT", "DELETE", "POST", "GET"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "X-Requested-With", "Access-Control-Allow-Origin", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		AllowAllOrigins:  true,
 		MaxAge:           12 * time.Hour,
-	}))
+	}))*/
 
 	for _, x := range routes {
 		for _, route := range x {
 			if route.RequireAuth {
-				Assign(route.Assign, middleware.Authorize(middleware.AbortOnError(route.Action)), route.URI, router)
+				Assign(route.Assign, middleware.CORS(middleware.Authorize(middleware.AbortOnError(route.Action))), route.URI, router)
 			} else {
-				Assign(route.Assign, middleware.AbortOnError(route.Action), route.URI, router)
+				Assign(route.Assign, middleware.CORS(middleware.AbortOnError(route.Action)), route.URI, router)
 			}
 		}
 	}
