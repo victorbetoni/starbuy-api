61f279c38b3a3532c2aa3f1fc9f4824ff39d8b51
Victor Betoni <victorbetoni@protonmail.com>
2022-11-08 21:11:47 -0300

    Já nao sei mais

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -36,6 +36,11 @@ func CORS() gin.HandlerFunc {
 
 func AbortOnError(next util.HandlerFunc) util.HandlerFunc {
 	return func(c *gin.Context) (int, error) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "h")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
 		if status, err := next(c); err != nil {
 			c.AbortWithStatusJSON(status, gin.H{"status": false, "message": err.Error()})
 			return 0, nil
